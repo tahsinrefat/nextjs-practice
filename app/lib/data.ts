d54ec8bb6ad1b8a88fd705483e3f44d70e8d780e
tahsinrefat <tahsin92refat@gmail.com>
2025-08-26 11:13:16 +0600

    chore: finished till chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -72,7 +72,6 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
-
     return {
       numberOfCustomers,
       numberOfInvoices,
